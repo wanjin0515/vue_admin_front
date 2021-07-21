5ee9432ac6058e73f62cd61bd13ac3830e2c1294
jinwan <409331530@qq.com>
2021-07-21 21:55:33 +0800

    集成js-cookie

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 
+import Vue from 'vue'
+import Cookies from 'js-cookie'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
 
-import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
