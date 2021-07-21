8de320f22cabc0726d9f0e039f47815753cdc8b5
jinwan <409331530@qq.com>
2021-07-21 21:35:30 +0800

    集成element-ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
